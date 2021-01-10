3d06b4e841386680bc77d3444e3dc2ad9ef00b95
张广汇 <1434386636@qq.com>
2021-01-10 19:22:31 +0800

    first commit

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import sort.impl.*;
-import utils.AlgorithmUtil;
-
-public class Main {
-    public static void main(String[] args) {
-        int[] nums = AlgorithmUtil.getDisorderNums(90000, 10000);
-        AlgorithmUtil.check(nums, new HeapSort());
-    }
-}
